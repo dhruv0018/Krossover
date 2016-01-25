27e8f9ac41a24da0acf679e73a386c82b60610cf
Dhruv <dhruv.motiani@structuredweb.com>
2016-01-24 21:29:04 -0500

    2 tests: one for home page and one for basketball page

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System.Diagnostics;
+
+namespace Krossover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                var driver = new FirefoxDriver();
+                driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(2));
+                
+                TestHomePage test = new TestHomePage();
+                test.testHomePage(driver);
+
+                TestBasketballPage test2 = new TestBasketballPage();
+                test2.testBasketballPage(driver);
+                
+        }
+
+        /// <summary>
+        /// Write to log file
+        /// </summary>
+        public void log(string logText)
+        {
+            // Create a writer and open the file
+            StreamWriter log;
+
+            if (!File.Exists("logfile.txt"))
+            {
+                log = new StreamWriter("logfile.txt");
+            }
+            else
+            {
+                log = File.AppendText("logfile.txt");
+            }
+
+            // Write to the file
+            log.WriteLine(DateTime.Now);
+            log.WriteLine(logText);
+            log.WriteLine();
+
+            log.Close();
+        }
+    }
+}
---
+++ ConsoleApplication2/TestHomePage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Collections.ObjectModel;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Support.UI;
+using System.Diagnostics;
+
+namespace Krossover
+{
+    class TestHomePage : Program
+    {
+        /// <summary>
+        /// Test all links on the Krossover home page
+        /// </summary>
+        public void testHomePage(FirefoxDriver driver)
+        {
+            string homePage = "http://www.krossover.com/";
+            driver.Navigate().GoToUrl(homePage);
+            Thread.Sleep(1000);
+
+            //find all links on the page
+            IList<IWebElement> links = driver.FindElementsByTagName("a");
+
+            //loop through all of the links on the page
+            foreach (IWebElement link in links)
+            {
+                try
+                {
+                    //check if any of the links return a 404
+                    driver.FindElementByXPath("//div[contains(@class, '404')]");
+                    log(link.GetAttribute("href") + " is broken.  Returned 404");
+                }
+                catch
+                {
+                    //continue to the next link
+                    continue;
+                }
+
+            }
+            driver.Quit(); //kill the driver
+        }
+    }
+}
